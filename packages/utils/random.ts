f3a29a6ba616153a72ee56e72e43a30add24ecfe
Shen <ihaiyon@gmail.com>
2023-03-11 10:14:30 +0800

    docs: update comments * use chatgpt suggestions

--- packages/utils/random.ts
+++ packages/utils/random.ts
@@ -1,8 +1,17 @@
+/**
+ * Generates a random string of specified length.
+ * @param len Length of the generated string. Defaults to 22.
+ */
 export const randomString = (len = 22): string => {
+  // Define character set
   const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-';
+  // Maximum position is one less than the length of the character set
   const maxPos = alphabet.length;
   let str = '';
   for (let i = 0; i < len; i++) {
+    // Generate a random number between 0 and 1 using Math.random(),
+    // multiply it by the maximum position to get a random index within the character set range,
+    // use charAt() function to get the corresponding character and append it to the string.
     str += alphabet.charAt(Math.floor(Math.random() * maxPos));
   }
   return str;
--- packages/utils/window-exists.ts
+++ packages/utils/window-exists.ts
@@ -1,1 +1,5 @@
+/**
+ * Checks if the `window` object is defined, indicating whether or not the code is being run in a browser environment.
+ * @returns A boolean value indicating whether or not the `window` object is defined.
+ */
 export default (): boolean => typeof window !== 'undefined';
